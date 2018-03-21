21fa0f79c6615cb7fb1f00cfcad8909bc6f04e56
Samuel Linde <samuel@dontblink.se>
2018-03-21 14:21:53 +0100

    Testing FP package

--- node.lua
+++ node.lua
@@ -2,6 +2,19 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local loader = require "loader"
 
+-- Listen for external triggers
+-- util.data_mapper {
+--     reset = function()
+--         timer = initTimer
+--     end,
+--     start = function()
+--         timerStatus = 'running'
+--     end,
+--     stop = function()
+--         timerStatus = 'stopped'
+--     end
+-- }
+
 function node.render()
     for name, module in pairs(loader.modules) do
         module.draw()
