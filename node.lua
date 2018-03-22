d881920630239d43f56ea52b974515dfed98c3e1
Samuel Linde <samuel@dontblink.se>
2018-03-22 12:06:43 +0100

    test

--- node.lua
+++ node.lua
@@ -20,11 +20,11 @@ local active_module = loader.modules[0]
 function node.render()
     gl.clear(0, 0, 0, 1)
     active_module.draw()
-    for name, module in pairs(loader.modules) do
-        print(name)
-        -- module.draw()
-        -- font:write(100, start_y, name, 60, 1, 1, 1, 1)
-        -- font:write(400, start_y, tostring(module), 60, 1, 1, 1, 1)
-        -- start_y = start_y + 100
-    end
+    font:write(100, 100, loader.modules[0], 60, 1, 1, 1, 1)
+    -- for name, module in pairs(loader.modules) do
+    --     print(name)
+    --     -- module.draw()
+    --     -- font:write(400, start_y, tostring(module), 60, 1, 1, 1, 1)
+    --     -- start_y = start_y + 100
+    -- end
 end
