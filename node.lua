6e92557aa01d67a112a20cafea2177b52296d3a3
Samuel Linde <samuel@dontblink.se>
2018-03-22 12:20:04 +0100

    test

--- node.lua
+++ node.lua
@@ -2,7 +2,7 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local loader = require "loader"
 local font = resource.load_font('robotob.ttf')
-local active_module, active_module_name = pairs(loader)
+local active_module = 'slideshow'
 
 -- Listen for external triggers
 -- util.data_mapper {
@@ -20,11 +20,13 @@ local active_module, active_module_name = pairs(loader)
 function node.render()
     gl.clear(0, 0, 0, 1)
     -- active_module.draw()
-    font:write(100, 100, tostring(active_module), 60, 1, 1, 1, 1)
-    -- for name, module in pairs(loader.modules) do
-    --     print(name)
-    --     -- module.draw()
+    -- font:write(100, 100, tostring(active_module), 60, 1, 1, 1, 1)
+    for name, module in pairs(loader.modules) do
+        print(name)
+        if name == active_module
+            module.draw()
+        end
     --     -- font:write(400, start_y, tostring(module), 60, 1, 1, 1, 1)
     --     -- start_y = start_y + 100
-    -- end
+    end
 end
