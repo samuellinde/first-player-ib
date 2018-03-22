2e747ecb86e3c090226110874001f6490edbded2
Samuel Linde <samuel@dontblink.se>
2018-03-23 00:26:33 +0100

    test

--- node.lua
+++ node.lua
@@ -5,7 +5,13 @@ local font = resource.load_font('robotob.ttf')
 local active_module = 'slideshow'
 local current_track = ''
 local current_track_width = 0
-local current_track_x = 0
+local current_track_x = 30
+local current_track_y = 30
+local current_track_size = 30
+
+local track_overlay = resource.create_colored_texture(0, 0, 0, 1)
+local track_overlay_alpha = 0.5
+local track_overlay_padding = 5
 
 -- Listen for external triggers
 util.data_mapper {
@@ -16,9 +22,17 @@ util.data_mapper {
 
 util.json_watch('config.json', function(config)
     current_track = 'Spelas: ' .. config.playing
-    current_track_width = font:width(current_track, 30)
-    current_track_x = NATIVE_WIDTH / 2 - current_track_width / 2
+    current_track_width = font:width(current_track, current_track_size)
+    -- current_track_x = NATIVE_WIDTH / 2 - current_track_width / 2
 end)
+
+local function draw_overlay()
+    local p = track_overlay_padding
+    local overlay_x1, overlay_y1 = current_track_x - p, current_track_y - p
+    local overlay_x2 = current_track_x + current_track_width + p
+    local overlay_y2 = current_track_y + current_track_size + p
+    track_overlay:draw(overlay_x1, overlay_y1, overlay_x2, overlay_y2, track_overlay_alpha)
+end
 
 function node.render()
     gl.clear(0, 0, 0, 1)
@@ -29,5 +43,6 @@ function node.render()
             module.unload()
         end
     end
-    font:write(current_track_x, 980, current_track, 30, 1, 1, 1, 1)
+    draw_overlay()
+    font:write(current_track_x, current_track_y, current_track, current_track_size, 1, 1, 1, 1)
 end
