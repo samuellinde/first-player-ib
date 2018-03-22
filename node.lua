1844576d93ea450d74067fc7f8ae9e5e45be072e
Samuel Linde <samuel@dontblink.se>
2018-03-23 00:12:26 +0100

    test

--- node.lua
+++ node.lua
@@ -3,6 +3,9 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 local loader = require "loader"
 local font = resource.load_font('robotob.ttf')
 local active_module = 'slideshow'
+local current_track = ''
+local current_track_width = 0
+local current_track_x = 0
 
 -- Listen for external triggers
 util.data_mapper {
@@ -10,6 +13,12 @@ util.data_mapper {
         active_module = module
     end
 }
+
+util.json_watch('config.json', function(json)
+    current_track = config.playing
+    current_track_width = font:width(current_track, 60)
+    current_track_x = NATIVE_WIDTH / 2 - current_track_width / 2
+end)
 
 function node.render()
     gl.clear(0, 0, 0, 1)
@@ -20,4 +29,5 @@ function node.render()
             module.unload()
         end
     end
+    font:write(current_track_x, 980, current_track, 60, 1, 1, 1, 1)
 end
