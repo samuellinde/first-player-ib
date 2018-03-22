08d5c017939e6e93e461555f88d96bf38dd2d8ba
Samuel Linde <samuel@dontblink.se>
2018-03-22 12:02:27 +0100

    test

--- node.lua
+++ node.lua
@@ -2,7 +2,7 @@ gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
 local loader = require "loader"
 local font = resource.load_font('robotob.ttf')
-local active_module = ''
+local active_module = CHILDS[0]
 local start_y = 100
 
 -- Listen for external triggers
@@ -20,10 +20,12 @@ local start_y = 100
 
 function node.render()
     gl.clear(0, 0, 0, 1)
+    active_module.draw()
     for name, module in pairs(loader.modules) do
+        print name
         -- module.draw()
-        font:write(100, start_y, name, 60, 1, 1, 1, 1)
-        font:write(400, start_y, tostring(module), 60, 1, 1, 1, 1)
-        start_y = start_y + 100
+        -- font:write(100, start_y, name, 60, 1, 1, 1, 1)
+        -- font:write(400, start_y, tostring(module), 60, 1, 1, 1, 1)
+        -- start_y = start_y + 100
     end
 end
