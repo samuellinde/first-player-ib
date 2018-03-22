52815588e71712343a6d562fe941c1fcba5c54fc
Samuel Linde <samuel@dontblink.se>
2018-03-22 12:26:59 +0100

    test

--- node.lua
+++ node.lua
@@ -5,28 +5,20 @@ local font = resource.load_font('robotob.ttf')
 local active_module = 'slideshow'
 
 -- Listen for external triggers
--- util.data_mapper {
---     reset = function()
---         timer = initTimer
---     end,
---     start = function()
---         timerStatus = 'running'
---     end,
---     stop = function()
---         timerStatus = 'stopped'
---     end
--- }
+util.data_mapper {
+    swap = function(module)
+        active_module = module
+    end
+}
 
 function node.render()
     gl.clear(0, 0, 0, 1)
-    -- active_module.draw()
-    -- font:write(100, 100, tostring(active_module), 60, 1, 1, 1, 1)
     for name, module in pairs(loader.modules) do
         print(name)
         if name == active_module then
             module.draw()
+        else
+            module.unload()
         end
-    --     -- font:write(400, start_y, tostring(module), 60, 1, 1, 1, 1)
-    --     -- start_y = start_y + 100
     end
 end
