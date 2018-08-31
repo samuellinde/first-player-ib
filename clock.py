e70c5c5561618f5b155c2bfb6ff57f3817b826cb
Samuel Linde <samuel@dontblink.se>
2018-08-31 19:32:31 +0200

    Pytz

--- clock.py
+++ clock.py
@@ -1,9 +1,11 @@
 import socket
 import datetime
+from pytz import timezone
 
-now = datetime.datetime.now()
 
-now_str = now.strftime('%H:%M', 'Europe/Stockholm')
+tz = timezone('Europe/Stockholm')
+now = datetime.datetime.now(tz)
+now_str = now.strftime('%H:%M')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto('fp-package/clock/%s' % now_str, ('127.0.0.1', 4444))
