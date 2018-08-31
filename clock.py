1a422aba177228ebaf5450e8b363ed54ce19360b
Samuel Linde <samuel@dontblink.se>
2018-08-31 19:34:36 +0200

    Test

--- clock.py
+++ clock.py
@@ -8,5 +8,5 @@ now = datetime.datetime.now(tz)
 now_str = now.strftime('%H:%M')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.sendto('fp-package/clock/%s' % now_str, ('127.0.0.1', 4444))
+sock.sendto('clock/%s' % now_str, ('127.0.0.1', 4444))
 print now_str
